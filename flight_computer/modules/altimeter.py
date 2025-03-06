67f812aad92504f6db34048fa9edc0ee56d645eb
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-03-06 16:22:01 +1300

    Updated bmp280 library and change to spi

--- flight_computer/modules/altimeter.py
+++ flight_computer/modules/altimeter.py
@@ -1,10 +1,12 @@
-from machine import I2C, Pin
-import flight_computer.libs.bmp280 as bmp280
+from machine import Pin, SPI
+from flight_computer.libs.bmp280 import BMP280SPI
 from flight_computer.modules.sdcard_manager import SDCardManager
 from flight_computer.modules.error_logger import log_error
 
-I2C_SDA_PIN = 0
-I2C_SCL_PIN = 1
+SPI_SCK_PIN = Pin(2)
+SPI_TX_PIN = Pin(3)
+SPI_RX_PIN = Pin(4)
+SPI_CS_PIN = Pin(5, Pin.OUT, value=1)
 
 
 class Altimeter:
@@ -13,15 +15,16 @@ class Altimeter:
         sd_manager: SDCardManager,
         sd_file_path: str = "/sd/altimeter_data.txt",
     ):  # Functionality to initialise file path
-        self.i2c = I2C(0, sda=Pin(I2C_SDA_PIN), scl=Pin(I2C_SCL_PIN), freq=400000)
-        self.bmp = bmp280.BMP280(self.i2c)
+        self.spi = SPI(0, sck=SPI_SCK_PIN, mosi=SPI_TX_PIN, miso=SPI_RX_PIN)
+        self.bmp = BMP280SPI(self.spi, SPI_CS_PIN)
         self.sd_manager = sd_manager
         self.sd_file_path = sd_file_path
 
     def _read_data(self):
         try:
-            temperature = self.bmp.temperature
-            pressure = self.bmp.pressure
+            readout = self.bmp.measurements
+            temperature = readout['t']
+            pressure = readout['p']
             return temperature, pressure
         except Exception as e:
             log_error(f"ERROR: Reading sensor data failed: {e}")
@@ -31,11 +34,11 @@ class Altimeter:
         temperature, pressure = self._read_data()
         if temperature is not None and pressure is not None:
             altimeter_data = (
-                f"Temperature: {temperature:.2f} °C, Pressure: {pressure:.2f} Pa"
+                f"Temperature: {temperature:.2f} °C, Pressure: {pressure:.2f} hPa"
             )
             self.sd_manager.log(self.sd_file_path, altimeter_data)
 
 
-# BMP280 library: https://github.com/dafvid/micropython-bmp280/tree/master
+# BMP280 library: https://github.com/flrrth/pico-bmp280
 # SD card format is FAT32
 # SD card library: https://raw.githubusercontent.com/micropython/micropython-lib/master/micropython/drivers/storage/sdcard/sdcard.py
