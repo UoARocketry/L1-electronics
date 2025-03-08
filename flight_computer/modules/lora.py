f9913bf4bf99ab61f704317d337745c7b306eeb5
long715 <long715@aucklanduni.ac.nz>
2025-03-09 10:27:45 +1300

    lora alt

--- flight_computer/modules/lora.py
+++ flight_computer/modules/lora.py
@@ -1,5 +1,7 @@
-from flight_computer.libs.ulora import LoRa
-from flight_computer.modules.error_logger import log_error
+from flight_computer.libs.lora import LoRa
+from machine import SPI, Pin
+
+# from flight_computer.modules.error_logger import log_error
 
 LORA_CHANNEL = 0
 LORA_SCK = 18
@@ -16,16 +18,20 @@ LORA_RX_ADDR = 1
 
 class LORA(object):
     def __init__(self):
+        self.spi = SPI(
+            0,
+            baudrate=10000000,
+            sck=Pin(LORA_SCK, Pin.OUT, Pin.PULL_DOWN),
+            mosi=Pin(LORA_MOSI, Pin.OUT, Pin.PULL_UP),
+            miso=Pin(LORA_MISO, Pin.IN, Pin.PULL_UP),
+        )
         self.lora = LoRa(
-            (LORA_CHANNEL, LORA_SCK, LORA_MOSI, LORA_MISO),
-            LORA_DIO0,
-            LORA_TX_ADDR,
-            LORA_CS,
-            LORA_RST,
+            self.spi,
+            cs=Pin(LORA_CS, Pin.OUT),
         )
 
     def transmit_message(self, message: str):
-        self.lora.send(message, LORA_RX_ADDR)
-        if not self.lora.wait_packet_sent():
-            # log error in the SD card
-            log_error("ERROR: Failed to transmit from LoRa")
+        self.lora.send(message)
+        # if not self.lora.wait_packet_sent():
+        # log error in the SD card
+        # log_error("ERROR: Failed to transmit from LoRa"
--- flight_computer/test_scripts/lora.py
+++ flight_computer/test_scripts/lora.py
@@ -6,27 +6,32 @@ Test script for the radio/lora - mainly testing transmission
 from time import sleep
 
 # additional installations: https://github.com/martynwheeler/u-lora
-from flight_computer.libs.ulora import LoRa
+from flight_computer.libs.lora import LoRa
 from flight_computer.modules.lora import *
-from flight_computer.modules.error_logger import log_debug
 
+# from flight_computer.modules.error_logger import log_debug
+
+spi = SPI(
+    0,
+    baudrate=10000000,
+    sck=Pin(LORA_SCK, Pin.OUT, Pin.PULL_DOWN),
+    mosi=Pin(LORA_MOSI, Pin.OUT, Pin.PULL_UP),
+    miso=Pin(LORA_MISO, Pin.IN, Pin.PULL_UP),
+)
 lora = LoRa(
-    (LORA_CHANNEL, LORA_SCK, LORA_MOSI, LORA_MISO),
-    LORA_DIO0,
-    LORA_TX_ADDR,
-    LORA_CS,
-    LORA_RST,
+    spi,
+    cs=Pin(LORA_CS, Pin.OUT),
 )
 
 while True:
     # waits for prev msgs to be transmitted, if timeout, reset the buffer
     # and send new packet
     # pts of failure: some reason none of the packets gets sent
-    lora.send("testing", LORA_RX_ADDR)  # w/o ack
+    lora.send("testing")  # w/o ack
 
     # explicitly wait for tx to finish cos we can only hope that
     # it did finish transmitting above, unless we test this with GS
-    if not lora.wait_packet_sent():
-        log_debug("ERROR: timeout transmission")
+    # if not lora.wait_packet_sent():
+    #     log_debug("ERROR: timeout transmission")
 
     sleep(0.5)
